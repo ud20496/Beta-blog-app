ed3b5c024f6cf46a8e0dbf1e14427a33099da25b
ud20496 <uditbhola018@gmail.com>
2017-08-26 19:57:28 +0530

    Add sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ before_action :require_same_user,only:[:edit,:update]
   def create
     @user =User.new(user_params)
     if @user.save
+      session[:user_id]=@user.id
       flash[:success]="Welcome to Beta Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to users_path(@user)
     else
       render 'new'
     end
