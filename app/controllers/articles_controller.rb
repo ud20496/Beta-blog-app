6a08e31c30287acc4f62d46b6d9ec463c816c451
ud20496 <uditbhola018@gmail.com>
2017-08-26 19:48:40 +0530

    Updated Complete Authentication System

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
     debugger
     @article = Article.new(article_params)
-    @article.user= User.first
+    @article.user= current_user
 
     if @article.save
     flash[:success]= "Articles was successfully created"
