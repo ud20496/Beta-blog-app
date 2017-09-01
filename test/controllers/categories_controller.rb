c3bea9ea3d3c7eccb6350c25080a6f2efcc47ddc
ud20496 <uditbhola018@gmail.com>
2017-09-02 01:08:29 +0530

    Add requirement to category

--- test/controllers/categories_controller.rb
+++ test/controllers/categories_controller.rb
@@ -3,6 +3,7 @@
 class CategoriesControllerTest < ActionController :: TestCase
   def setup
     @category= Category.create(name:"sports")
+    @user = User.create(username: "udit",email:"uditbhola018@gmail.com",password: "Ud20496",admin:true)
   end
 
   test "should get categories index" do
@@ -11,12 +12,21 @@ class CategoriesControllerTest < ActionController :: TestCase
   end
 
   test"should get new" do
+    session[:user_id]=@user.id
     get :new
     assert_response :success
   end
 
   test"should get show" do
-    get(:show ,{ 'id'=>@category.id})
+    get(:show, { 'id' => @category.id})
     assert_response :success
   end
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "sports"}
+
+    end
+    assert_redirected_to categories_path
+  end
+
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-test"get new category form and create category"do
+
+  test"get new category form and create category"do
   get new_category_path
   assert_template 'categories/new'
   assert_difference 'Category.count',1 do
@@ -11,7 +12,7 @@ test"get new category form and create category"do
   assert_template 'categories/index'
   assert_match "sports",response.body
 end
-  test"invalid category submission resu lts in failure"do
+  test"invalid category submission results in failure"do
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count'do
